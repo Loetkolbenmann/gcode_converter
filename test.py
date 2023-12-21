83677cb5b693899f1af0dc94ea241a6befab825f
Daniel Heink [Student] <oq88iqal@lhft.de>
2023-12-21 17:07:26 +0100

    add visualisation and make gcode_to_array more robust

--- test.py
+++ test.py
@@ -3,15 +3,11 @@
 This program takes gcode and returns the individual extrusions that the printhead lays down. 
 
 """
-from gcode_converter import gcode_to_array
+from gcode_converter import gcode_to_array, visualize_paths
 from pathlib import Path
 import matplotlib.pyplot as plt
+import numpy as np
 
-gcode = gcode_to_array(Path('input_gcode','grbl laser.nc'))
+gcode = gcode_to_array(Path('input_gcode','slab_gcode.gcode'))
 
-#visualize 
-plt.figure().add_subplot(projection = '3d')
-for path in gcode:
-    for move in path:
-        plt.scatter(*move[:3])
-        
+visualize_paths(gcode,include_positioning_move=True)
